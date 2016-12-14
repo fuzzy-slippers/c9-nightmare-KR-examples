42915bb2c8aa30638c0f5034d1764f9885990c78
Zach Friedman <zif@umd.edu>
2016-12-14 15:48:32 -0500

    Removed folders and js files not being used by the project. Also changed all single quotes to double quotes (still was working after)

--- resdemo1-example.js
+++ resdemo1-example.js
@@ -1,15 +1,18 @@
-const Nightmare = require('nightmare');
+const Nightmare = require("nightmare");
 const nightmare = Nightmare({ show: false });
-const dateFormat = require('dateformat'); // get pretty dates, times
-const fs = require('fs');
+const dateFormat = require("dateformat"); // get pretty dates, times
+const fs = require("fs");
 
 //some helpful variables to record
 
 //used in the file name to indicate current module
-const currModule = 'Awd';
+const currModule = "Awd";
 
 //store current date time - used for foldernames so cant change, need time when program first run
 const now = new Date();
+
+//believe I figured out this is the URL (inside the IFRAME) used to create a new award
+const newAwardURL = "https://res-demo1.kuali.co/kc-dev/awardHome.do?methodToCall=docHandler&command=initiate&docTypeName=AwardDocument&returnLocation=https://res-demo1.kuali.co:/kc-dev%2Fkc-krad%2FlandingPage%3FviewId%3DKc-LandingPage-RedirectView#topOfForm";
 
 
 // start up nightmare
@@ -23,35 +26,35 @@ nightmare
   
 // LOGIN TO KUALI
 // --------------------------------------------------.
-  .goto('http://res-demo1.kuali.co/kc-dev/')
+  .goto("http://res-demo1.kuali.co/kc-dev/")
   .screenshot(getUniqScreenshotName())
 
-  .type('#username', 'quickstart')
+  .type("#username", "quickstart")
   .screenshot(getUniqScreenshotName())
 
-  .type('#password', 'password')
+  .type("#password", "password")
   .screenshot(getUniqScreenshotName())
   
-  .click('#login') //will open the Kuali Research welcome page after login
-  .wait('#Uif-ApplicationHeader-Wrapper') // wait until the banner at the top of the Kuali Wrapper on the main page loads
+  .click("#login") //will open the Kuali Research welcome page after login
+  .wait("#Uif-ApplicationHeader-Wrapper") // wait until the banner at the top of the Kuali Wrapper on the main page loads
   .screenshot(getUniqScreenshotName())
 
 // CREATE NEW AWARD
 // --------------------------------------------------.
 
   // go to award add page URL - Going directly to inner IFRAME (that is the real award form), otherwise the selectors nightmareJS doesnt work to find the input fields
-  .goto('https://res-demo1.kuali.co/kc-dev/awardHome.do?methodToCall=docHandler&command=initiate&docTypeName=AwardDocument&returnLocation=https://res-demo1.kuali.co:/kc-dev%2Fkc-krad%2FlandingPage%3FviewId%3DKc-LandingPage-RedirectView#topOfForm')
+  .goto(newAwardURL)
   .screenshot(getUniqScreenshotName())
   
   
 // AWARD (AWARD TAB) - DOCUMENT OVERVIEW SECTION
 // --------------------------------------------------.  
   //enter Document Overview - Description text field
-  .type('#document\\.documentHeader\\.documentDescription', 'NightmareJS Entered ' +  dateFormat(now))
+  .type("#document\\.documentHeader\\.documentDescription", "NightmareJS Entered " +  dateFormat(now))
   .screenshot(getUniqScreenshotName()) 
   
     //enter Document Overview - Explanation text area
-  .type('#document\\.documentHeader\\.explanation', 'NightmareJS Entered this automatically on ' + dateFormat(now) +'!')
+  .type("#document\\.documentHeader\\.explanation", "NightmareJS Entered this automatically on " + dateFormat(now) +"!")
   .screenshot(getUniqScreenshotName())
 
   
@@ -59,10 +62,10 @@ nightmare
 // AFTER RUNNING THE TEST - CLEANUP 
 // --------------------------------------------------.  
   .then(function (result) {
-    console.log('Test Finished Running(see screenshots like [' + getUniqScreenshotName() + ']! \nany results? ' + result)
+    console.log("Test Finished Running(see screenshots like [" + getUniqScreenshotName() + "]! \nany results? " + result)
   })
   .catch(function (error) {
-    console.error('Search failed:', error);
+    console.error("Search failed:", error);
   });
   
 
@@ -70,7 +73,8 @@ nightmare
 
 
 
-//set up filename (and folder path)
+//set up filename (and folder path) - broke this out into a function 
+//so people could just copy/paste the screenshot line over and over
 function getUniqScreenshotName() {
   //use the current date/time "now" variable captured when the program first ran 
   //for the folder names
@@ -78,11 +82,11 @@ function getUniqScreenshotName() {
   const startTime = dateFormat(now, "h_MM_ss_TT");
   
   //folder to store the screenshots
-  const baseScreenshotFolder = 'screenshots';
-  const subFolder =  todaysDate + '__' + startTime;
-  const folderpath = baseScreenshotFolder + '/' + subFolder + '/';
+  const baseScreenshotFolder = "screenshots";
+  const subFolder =  todaysDate + "__" + startTime;
+  const folderpath = baseScreenshotFolder + "/" + subFolder + "/";
   //create folders if they dont exist
   if (!fs.existsSync(baseScreenshotFolder)){fs.mkdirSync(baseScreenshotFolder);}
   if (!fs.existsSync(folderpath)){fs.mkdirSync(folderpath);}  
-  return folderpath + currModule + new Date().valueOf() + '.png';
+  return folderpath + currModule + new Date().valueOf() + ".png";
 }
