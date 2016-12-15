ee29dd277a9c554b348c8b01547d0aa4bc5a52f3
Zach Friedman <zif@umd.edu>
2016-12-15 10:50:46 -0500

    Set up working example of filling out required fields in award tab and one field with lookup on contacts tab of the award

--- README.md
+++ README.md
@@ -7,23 +7,21 @@
     ----------------------------------------------------------------- 
 
 
-Welcome to your Node.js project on Cloud9 IDE!
+Welcome to your ~~Node.js project~~ Kuali ResDemo1 Automated Testing (Node.js/NightmareJS) project using the Cloud9 IDE (and servers)!
 
-This chat example showcases how to use `socket.io` with a static `express` server.
+Here are instructions for runing the program (example is the award module):
 
-## Running the server
+## Running the automated test program/script
 
-1) Open `server.js` and start the app by clicking on the "Run" button in the top menu.
+1) Open `resdemo1-example.js` (from the left hand menu) and start the app by clicking on the "Run" button in the top menu. You might need to try running it once to get the tab at the bottom, but you'll need to make sure the "resdemo1-example" tab at the bottom of the screen, is set with "Runner: xvfb_node" on the right hand side of that bottom tab (you'd have to select it from the dropdown list)
 
-2) Alternatively you can launch the app from the Terminal:
+2) Alternatively you can launch the app from the Linux Terminal (bash tab at the bottom):
 
-    $ node server.js
+    $ xvfb-run node resdemo1-example.js
 
-
-
-For Nightmare using syntax like:
-
-xvfb-run node yahoo-example.js
+Once it's runing, you should see a folder created underneath the "screenshots" folder on the left hand list of files/folders with todays date and time. Inside that you should see screenshots start to appear as it runs through the automated test/automatically does stuff
 
 
-Once the server is running, open the project in the shape of 'https://projectname-username.c9users.io/'. As you enter your name, watch the Users list (on the left) update. Once you press Enter or Send, the message is shared with all connected clients.
+
+
+~~Once the server is running, open the project in the shape of 'https://projectname-username.c9users.io/'. As you enter your name, watch the Users list (on the left) update. Once you press Enter or Send, the message is shared with all connected clients.~~
--- resdemo1-example.js
+++ resdemo1-example.js
@@ -21,44 +21,155 @@ nightmare
 // NIGHTMARE/BROWSER SETTINGS
 // --------------------------------------------------.
 
-  // first set the browser size/resolution to 1024X768
-  .viewport(1024, 768)
+  // first set the browser size/resolution to 1024X2200 (the reason its 2200 is that way we capture the whole long scrolling page down to the save button)
+  .viewport(1024, 2200)
   
 // LOGIN TO KUALI
 // --------------------------------------------------.
   .goto("http://res-demo1.kuali.co/kc-dev/")
-  .screenshot(getUniqScreenshotName())
+  .screenshot(getUniqScreenshotName()).wait(1) //wait one millisecond just so screenshots have different names
 
   .type("#username", "quickstart")
-  .screenshot(getUniqScreenshotName())
+  .screenshot(getUniqScreenshotName()).wait(1)
 
   .type("#password", "password")
-  .screenshot(getUniqScreenshotName())
+  .screenshot(getUniqScreenshotName()).wait(1)
   
   .click("#login") //will open the Kuali Research welcome page after login
   .wait("#Uif-ApplicationHeader-Wrapper") // wait until the banner at the top of the Kuali Wrapper on the main page loads
-  .screenshot(getUniqScreenshotName())
+  .screenshot(getUniqScreenshotName()).wait(1)
 
 // CREATE NEW AWARD
 // --------------------------------------------------.
 
   // go to award add page URL - Going directly to inner IFRAME (that is the real award form), otherwise the selectors nightmareJS doesnt work to find the input fields
   .goto(newAwardURL)
-  .screenshot(getUniqScreenshotName())
+  .screenshot(getUniqScreenshotName()).wait(1)
   
   
 // AWARD (AWARD TAB) - DOCUMENT OVERVIEW SECTION
 // --------------------------------------------------.  
   //enter Document Overview - Description text field
   .type("#document\\.documentHeader\\.documentDescription", "NightmareJS Entered " +  dateFormat(now))
-  .screenshot(getUniqScreenshotName()) 
+  .screenshot(getUniqScreenshotName()).wait(1) 
   
     //enter Document Overview - Explanation text area
   .type("#document\\.documentHeader\\.explanation", "NightmareJS Entered this automatically on " + dateFormat(now) +"!")
-  .screenshot(getUniqScreenshotName())
+  .screenshot(getUniqScreenshotName()).wait(1)
+  
+  
+// AWARD (AWARD TAB) - DETAILS AND DATES SECTION
+// --------------------------------------------------.   
+  //Select from dropdown Transaction Type 1 (Administrative Amendment)
+  .select("#document\\.awardList\\[0\\]\\.awardTransactionTypeCode", 1)
+  .screenshot(getUniqScreenshotName()).wait(1)  
 
+// AWARD (AWARD TAB) - INSTITUTION SECTION
+// --------------------------------------------------.   
+
+  //Select from dropdown Award Status 2 (Inactive)
+  .select("#document\\.awardList\\[0\\]\\.statusCode", 2)
+  .screenshot(getUniqScreenshotName()).wait(1)  
+
+  //type lead unit number 000001
+  .type("#document\\.awardList\\[0\\]\\.unitNumber", "000001")
+  .screenshot(getUniqScreenshotName()).wait(1)
+
+  //enter award title "Award entered by nightmareJS " and todays date time
+  .type("#document\\.awardList\\[0\\]\\.title", "Award entered by nightmareJS " +  dateFormat(now))
+  .screenshot(getUniqScreenshotName()).wait(1)
+
+  //Select from dropdown Activity type 3 (Public Service)
+  .select("#document\\.awardList\\[0\\]\\.activityTypeCode", 3)
+  .screenshot(getUniqScreenshotName()).wait(1) 
+  
+  //Select from dropdown Award Type 11 (Gift)
+  .select("#document\\.awardList\\[0\\]\\.awardTypeCode", 11)
+  .screenshot(getUniqScreenshotName()).wait(1) 
+
+// AWARD (AWARD TAB) - SPONSOR SECTION
+// --------------------------------------------------.     
+  //enter sponsor code (using NSFs code of 000500)
+  .type("#document\\.awardList\\[0\\]\\.sponsorCode", "000500")
+  .screenshot(getUniqScreenshotName()).wait(1)  
   
 
+// AWARD (AWARD TAB) - TIME AND MONEY SECTION
+// --------------------------------------------------. 
+  //enter project end date of 07/28/2017
+  .type("#document\\.awardList\\[0\\]\\.awardAmountInfos\\[0\\]\\.finalExpirationDate", "07/28/2017")
+  .screenshot(getUniqScreenshotName()).wait(1)
+  
+// AWARD (AWARD TAB) - SAVE
+// --------------------------------------------------. 
+  //click the save button on the bottom
+  .click("input.globalbuttons")
+  //take a screenshot right away to hopefully catch the saving box
+  .screenshot(getUniqScreenshotName())
+  //wait three seconds for save to finish
+  .wait(3000)
+  //take another screenshot after the save just to make sure there wasn't an error displayed
+  .screenshot(getUniqScreenshotName())
+
+
+// AWARD CONTACTS TAB - CLICK ON CONTACTS TAB
+// --------------------------------------------------. 
+  //click on contacts tab
+  .click("input[name='methodToCall\\.headerTab\\.headerDispatch\\.save\\.navigateTo\\.contacts'")
+  //wait three seconds for contacts tab loading to finish
+  .wait(3000)
+  //take screenshot of contacts tab after it loaded
+  .screenshot(getUniqScreenshotName())
+  
+  //click expand all button to show all fields on the tab
+  .click("input[name='methodToCall\\.showAllTabs']")
+  //take a screenshot of expanded tabs
+  .screenshot(getUniqScreenshotName())
+
+// AWARD CONTACTS TAB - UNIT CONTACTS
+// --------------------------------------------------.
+  //click first on search magnifier (seems to be the only way/option)
+  .click("input[name='methodToCall\\.performLookup\\.\\(\\!\\!org\\.kuali\\.coeus\\.common\\.framework\\.person\\.KcPerson\\!\\!\\)\\.\\(\\(\\(personId\\:unitContactsBean\\.personId\\)\\)\\)\\.\\(\\(\\`unitContactsBean\\.personId\\:personId\\`\\)\\)\\.\\(\\(\\<\\>\\)\\)\\.\\(\\(\\[\\]\\)\\)\\.\\(\\(\\*\\*\\)\\)\\.\\(\\(\\^\\^\\)\\)\\.\\(\\(\\&\\&\\)\\)\\.\\(\\(\\/\\/\\)\\)\\.\\(\\(\\~\\~\\)\\)\\.\\(\\:\\:\\:\\:\\;\\;\\:\\:\\:\\:\\)\\.anchorUnitContacts']")
+  //wait 2 seconds for lookup screen to load
+  .wait(2000)
+  //take a screenshot of lookup screen after
+  .screenshot(getUniqScreenshotName())
+
+  //enter personid of 10009 (Rosemary Hanlon)
+  .type("#personId", "10009")
+  .screenshot(getUniqScreenshotName()).wait(1) 
+  
+  //PERSON LOOKUP SCREEN
+  //click search button
+  .click("input[name='methodToCall\\.search']")
+  //wait 2 seconds for search results to load
+  .wait(2000)
+  //take screenshot
+  .screenshot(getUniqScreenshotName())
+  
+  //click return value to return back to contacts page
+  .click("a[title='return valueKcPerson Id=10009 ']")
+  //wait 2 seconds for results to return
+  .wait(2000)
+  //take screenshot
+  .screenshot(getUniqScreenshotName())
+  
+  //BACK TO CONTACTS PAGE
+  //click the add button to add rosemary
+  .click("input[name='methodToCall\\.addUnitContact']")
+  //wait 2 seconds for search results to load
+  .wait(2000)
+  //take screenshot
+  .screenshot(getUniqScreenshotName())  
+  
+  //save the contacts info
+  .click("input[name='methodToCall\\.save']")
+  //wait 2 seconds for search results to load
+  .wait(2000)
+  //take screenshot
+  .screenshot(getUniqScreenshotName())  
+  
+  
 // AFTER RUNNING THE TEST - CLEANUP 
 // --------------------------------------------------.  
   .then(function (result) {
@@ -67,6 +178,8 @@ nightmare
   .catch(function (error) {
     console.error("Search failed:", error);
   });
+  
+  
   
 
 
